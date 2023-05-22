064cc46b146cf106993ff6e75f85377ccbbb2ef0
nyafiu <mtsgonzalezblanco@gmail.com>
2023-05-22 09:27:55 -0600

    import de g

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, g, render_template, request, jsonify
 from config import config
 from routes import Producto
 from flask_cors import CORS
