d6f86d0b3d6ec6101557748764d16a08b274a902
nyafiu <mtsgonzalezblanco@gmail.com>
2023-06-16 21:24:58 -0600

    arreglo de carrito

--- src/static/assets/js/bodegaV2.js
+++ src/static/assets/js/bodegaV2.js
@@ -1,22 +1,14 @@
-let todosLosProductos = [];
-let paginaActual = 1;
-const productosPorPagina = 10;
-
 const templateCard = document.getElementById("template-card").content;
 const templateFooter = document.getElementById("template-footer").content;
 const templateCarrito = document.getElementById("template-carrito").content;
 const cards = document.getElementById("cards");
 const items = document.getElementById("items");
-const footer = document.getElementById("footer");
+const footer = document.getElementById("footer")
 const fragment = document.createDocumentFragment();
-let carrito = {};
+let carrito = {}
 
-cards.addEventListener("click", e => {
-    addCarrito(e);
-});
-
-items.addEventListener("click", e => {
-    btnAccion(e);
+document.addEventListener("DOMContentLoaded", () => {
+    fetchData();
 });
 
 const fetchData = async () => {
@@ -24,23 +16,23 @@ const fetchData = async () => {
         const res = await fetch("api/productos/bodega");
         const data = await res.json();
 
-        todosLosProductos = data.productos; // Asignar los datos de productos a la variable todosLosProductos
-
-        mostrarBodega();
+        mostrarBodega(data);
     } catch (error) {
         console.log(error);
     }
 };
 
-const mostrarBodega = () => {
-    if (!Array.isArray(todosLosProductos)) {
-        console.error("La variable todosLosProductos no es un array válido");
-        return;
-    }
+cards.addEventListener("click", e =>{
+    addCarrito(e)
+})
 
-    const productosParaMostrar = todosLosProductos.slice((paginaActual - 1) * productosPorPagina, paginaActual * productosPorPagina);
+items.addEventListener("click", e =>{
+    btnAccion(e)
+})
 
-    productosParaMostrar.forEach(producto => {
+const mostrarBodega = data => {
+    data.productos.forEach(producto => {
+        console.log(producto);
         templateCard.querySelector('h1').textContent = producto.nombre;
         templateCard.querySelector('h3').textContent = producto.precio;
         templateCard.querySelector('p').textContent = producto.subcategoria;
@@ -49,15 +41,15 @@ const mostrarBodega = () => {
         const clone = templateCard.cloneNode(true);
         fragment.appendChild(clone);
     });
-
     cards.appendChild(fragment);
 };
 
-function mostrarMasProductosAlFinalizarScroll() {
-    if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
-        paginaActual++;
-        mostrarBodega();
+const addCarrito = e =>{
+    //console.log(e.target.classList.contains('btn-dark'))
+    if(e.target.classList.contains('btn-dark')){
+        setCarrito(e.target.parentElement)
     }
+    e.stopPropagation()
 }
 
 
@@ -78,37 +70,34 @@ const mostrarCarrito = () => {
     })
     items.appendChild(fragment)
 
-    items.appendChild(fragment);
-    mostrarFooter();
-};
+    mostrarFooter()
+}
 
 const mostrarFooter = () => {
-    footer.innerHTML = '';
-
-    if (Object.keys(carrito).length === 0) {
+    footer.innerHTML = ''
+    if (Object.keys(carrito).length === 0){
         footer.innerHTML = `
-            <th scope="row" colspan="5">Carrito Vacio</th>
-        `;
+        <th scope="row" colspan="5">Carrito Vacio</th>
+        `    
+        return
+    }   
+    const nCantidad = Object.values(carrito).reduce((acc, {cantidad}) => acc + cantidad,0)
+    const nPrecio = Object.values(carrito).reduce((acc, {cantidad, precio}) => acc + precio * cantidad,0)
+    //console.log(nPrecio)
 
-        return;
-    }
+    templateFooter.querySelectorAll('td')[0].textContent = nCantidad
+    templateFooter.querySelector('span').textContent = nPrecio
+    
+    const clone = templateFooter.cloneNode(true)
+    fragment.appendChild(clone)
+    footer.appendChild(fragment)
 
-    const nCantidad = Object.values(carrito).reduce((acc, { cantidad }) => acc + cantidad, 0);
-    const nPrecio = Object.values(carrito).reduce((acc, { cantidad, precio }) => acc + precio * cantidad, 0);
-
-    templateFooter.querySelectorAll('td')[0].textContent = nCantidad;
-    templateFooter.querySelector('span').textContent = nPrecio;
-
-    const clone = templateFooter.cloneNode(true);
-    fragment.appendChild(clone);
-    footer.appendChild(fragment);
-
-    const btnVaciar = document.getElementById('vaciar-carrito');
+    const btnVaciar = document.getElementById('vaciar-carrito')
     btnVaciar.addEventListener("click", () => {
-        carrito = {};
-        mostrarCarrito();
-    });
-};
+        carrito = {}
+        mostrarCarrito()
+    }) 
+}
 
 const setCarrito = objeto => {
     const producto = {
@@ -127,28 +116,19 @@ const setCarrito = objeto => {
 }
 const btnAccion = e => {
     if (e.target.classList.contains('btn-info')) {
-        const producto = carrito[e.target.dataset.id];
-        producto.cantidad++;
-        carrito[e.target.dataset.id] = { ...producto };
-        mostrarCarrito();
+        const producto = carrito[e.target.dataset.id]
+        producto.cantidad++
+        carrito[e.target.dataset.id] = {...producto}
+        mostrarCarrito()
     }
 
-    if (e.target.classList.contains('btn-danger')) {
-        const producto = carrito[e.target.dataset.id];
-        producto.cantidad--;
-
-        if (producto.cantidad === 0) {
-            delete carrito[e.target.dataset.id];
+    if(e.target.classList.contains('btn-danger')) {
+        const producto = carrito[e.target.dataset.id]
+        producto.cantidad--
+        if(producto.cantidad === 0){
+            delete carrito[e.target.dataset.id]
         }
-
-        mostrarCarrito();
+        mostrarCarrito()
     }
-
-    e.stopPropagation();
-};
-
-document.addEventListener("DOMContentLoaded", () => {
-    fetchData();
-    window.addEventListener('scroll', mostrarMasProductosAlFinalizarScroll);
-});
-
+    e.stopPropagation()
+}
